5dfa76fee33083e7a0664079b9d7690eb5895711
andreasblomqvist <adde-16@hotmail.com>
2019-11-20 15:45:15 +0100

    first commit

---
+++ Labb2_Program/Position.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Labb2_Program
+{
+    internal class Position
+    {
+        private double x;
+        public double X { get => x; set => x = value < 0 ? 0 : value; }
+
+        private double y;
+        public double Y { get => y; set => y = value < 0 ? 0 : value; }
+
+        public Position(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public double Length()
+        {
+            return Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2));
+        }
+
+        public bool Equals(Position p)
+        {
+            return X.Equals(p.X) && Y.Equals(p.Y);
+        }
+
+        public Position Clone()
+        {
+            return new Position(X, Y);
+        }
+
+        public override string ToString()
+        {
+            return "("+X+","+Y+")";
+        }
+
+        public static bool operator >(Position p1, Position p2)
+        {
+            if (p1.Length().Equals(p2.Length())) {
+                return p1.X > p2.X;
+            }
+            return p1.Length() > p2.Length();
+        }
+
+        public static bool operator <(Position p1, Position p2)
+        {
+            return p2 > p1;
+        }
+
+        public static Position operator +(Position p1, Position p2)
+        {
+            return new Position(p1.X + p2.X, p1.Y + p2.Y);
+        }
+
+        public static Position operator -(Position p1, Position p2)
+        {
+            double x = p1.X - p2.X;
+            double y = p1.Y - p2.Y;
+
+            if (x < 0)
+            {
+                x *= -1;
+            }
+            if (y < 0)
+            {
+                y *= -1;
+            }
+
+            return new Position(x, y);
+        }
+
+        public static double operator %(Position p1, Position p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+        }
+    }
+}
---
+++ Labb2_Program/SortedPosList.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+
+namespace Labb2_Program
+{
+    internal class SortedPosList
+    {
+        List<Position> posList { get; set; }
+        public SortedPosList() => posList = new List<Position>();
+
+        public int Count()
+        {
+            return posList.Count;
+        }
+
+        public void Add(Position pos)
+        {
+            if (posList.Count <= 0)
+            {
+                posList.Add(pos);
+            }
+            else
+            {
+                for (int i = 0; i < posList.Count; i++)
+                {
+                    if (posList[i] > pos)
+                    {
+                        posList.Insert(i, pos);
+                        return;
+                    }
+
+                }
+                posList.Add(pos);
+            }
+        }
+
+        public bool Remove(Position pos)
+        {
+            bool didRemove = false;
+
+            for (int i = 0; i < posList.Count; i++)
+            {
+                if (posList[i].Equals(pos))
+                {
+                    posList.RemoveAt(i);
+                    didRemove = true;
+                }
+            }
+            return didRemove;
+        }
+
+        public SortedPosList Clone()
+        {
+            SortedPosList copy = new SortedPosList();
+
+            foreach (Position position in posList)
+            {
+                Position pos = position.Clone();
+                copy.Add(pos);
+            }
+            return copy;
+        }
+
+        public SortedPosList CircleContent(Position centerPos, double radius)
+        {
+            SortedPosList posInsideCircle = new SortedPosList();
+
+            for (int i = 0; i < this.posList.Count; i++)
+            {
+                Position position = this.posList[i];
+                bool isInside = (position.X - centerPos.X) * (position.X - centerPos.X) + (position.Y - centerPos.Y) * (position.Y - centerPos.Y) < radius * radius;
+
+                if (isInside)
+                {
+                    posInsideCircle.Add(position.Clone());
+                }
+            }
+
+            return posInsideCircle;
+        }
+
+        public static SortedPosList operator +(SortedPosList sp1, SortedPosList sp2)
+        {
+            SortedPosList copiedList = sp1.Clone();
+            for (int i = 0; i < sp2.Count(); i++)
+            {
+                copiedList.Add(sp2.posList[i]);
+            }
+            return copiedList;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", posList);
+        }
+    }
+}
